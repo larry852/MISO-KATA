2986d8e86ef2503cbcb2f500ebb4a84ea4aa8b07
David <davand20@hotmail.com>
2020-03-15 17:56:58 -0500

    Paso1 - Un numero (Rojo)

--- test.py
+++ test.py
@@ -4,5 +4,8 @@ from main import Kata
 
 
 class KataTest(TestCase):
-    def test_sumar(self):
-        self.assertEqual(Kata.numberElements(""), 0, "String vacio")
+    def test_number_elements_0(self):
+        self.assertEqual(Kata.numberElements(""), 0, "String vacío")
+
+    def test_number_elements_1(self):
+        self.assertEqual(Kata.numberElements("0"), 1, "String con un numero")
