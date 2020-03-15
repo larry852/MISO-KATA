8a585de5d178645c67786553deafc86a2f01e15b
larry852 <larryportocarrero@gmail.com>
2020-03-15 18:50:27 -0500

    Paso2 - cadena dos numeros (Azul)

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ class KataTest(TestCase):
     def setUp(self):
         self.string_empty = ""
         self.string_1 = "0"
+        self.string_2 = "-1, 0"
 
     def test_number_elements_0(self):
         self.assertEqual(Kata.numberElements(self.string_empty), 0, "String vacío")
@@ -16,7 +17,7 @@ class KataTest(TestCase):
         self.assertEqual(Kata.numberElements(self.string_1), 1, "String con un numero")
 
     def test_number_elements_2(self):
-        self.assertEqual(Kata.numberElements("0,1"), 2, "String con dos numeros")
+        self.assertEqual(Kata.numberElements(self.string_2), 2, "String con dos numeros")
 
     def test_number_elements_N(self):
         string = "0,1,2,156,564"
@@ -29,4 +30,4 @@ class KataTest(TestCase):
         self.assertEqual(Kata.minElement(self.string_1), 0, "String con un numero")
 
     def test_min_element_2(self):
-        self.assertEqual(Kata.minElement("-1, 0"), -1, "String con dos numeros")
+        self.assertEqual(Kata.minElement(self.string_2), -1, "String con dos numeros")
