d7b8bbe7477b0c0bf7bb2c68e35e7fa6f53b286a
David <davand20@hotmail.com>
2020-03-15 18:20:29 -0500

    Paso1 - N numeros (Rojo)

--- test.py
+++ test.py
@@ -12,3 +12,7 @@ class KataTest(TestCase):
 
     def test_number_elements_2(self):
         self.assertEqual(Kata.numberElements("0,1"), 2, "String con dos numeros")
+
+    def test_number_elements_N(self):
+        string = "0,1,2,156,564"
+        self.assertEqual(Kata.numberElements(string), len(string.split(',')), "String con N numeros")
